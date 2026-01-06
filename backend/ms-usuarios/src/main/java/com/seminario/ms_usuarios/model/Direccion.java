0c480b0bb515b22e7f8ccebce727c885f5d5446b
Juan-Marco-Garces <jmarcogarces@gmail.com>
2026-01-06 16:32:56 -0300

    anotaciones en direcciones y vendedor

--- backend/ms-usuarios/src/main/java/com/seminario/ms_usuarios/model/Direccion.java
+++ backend/ms-usuarios/src/main/java/com/seminario/ms_usuarios/model/Direccion.java
@@ -1,16 +1,30 @@
 package com.seminario.ms_usuarios.model;
 
 import lombok.Data;
+import jakarta.persistence.*;
 
 @Data
+@Entity
+@Table(name = "direccion")
 public class Direccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     private String id;
+    @Column(name = "calle")
     private String calle;
+    @Column(name = "numero")
     private String numero;
+    @Column(name = "localidad")
     private String localidad;
+    @Column(name = "provincia")
     private String provincia;
+    @Column(name = "codigoPostal")
     private String codigoPostal;
+    @Column(name = "observaciones")
     private String observaciones;
+    @Column(name = "latitud")
     private Double latitud;
+    @Column(name = "longitud")
     private Double longitud;
 }
--- backend/ms-usuarios/src/main/java/com/seminario/ms_usuarios/model/Vendedor.java
+++ backend/ms-usuarios/src/main/java/com/seminario/ms_usuarios/model/Vendedor.java
@@ -1,12 +1,18 @@
 package com.seminario.ms_usuarios.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Entity
+@Table(name = "vendedor")
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Vendedor extends Usuario {
+    @Column(name = "nombreNegocio")
     private String nombreNegocio;
+    @Column(name = "nombreResponsable")
     private String nombreResponsable;
+    @Column(name = "apellidoResponsable")
     private String apellidoResponsable;
 }
