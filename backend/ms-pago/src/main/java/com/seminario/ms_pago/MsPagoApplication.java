c131fd4ee03035e3e86e68ac50185a9c26deab66
karenoggier <karenoggier@gmail.com>
2026-02-24 09:12:22 -0300

    fix: mini arreglo

--- backend/ms-pago/src/main/java/com/seminario/ms_pago/MsPagoApplication.java
+++ backend/ms-pago/src/main/java/com/seminario/ms_pago/MsPagoApplication.java
@@ -2,8 +2,10 @@ package com.seminario.ms_pago;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MsPagoApplication {
 
 	public static void main(String[] args) {
